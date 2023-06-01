8c479723a31246f12cb22c5b60c9c882cf696a8a
yw1211 <127832625+yw1211@users.noreply.github.com>
2023-06-01 19:52:16 +0900

    implement hw5

--- figure_main.py
+++ figure_main.py
@@ -7,5 +7,9 @@ width, height = myline.get_length()
 try:
     rectangle = figure.area_rectangle(width, height)
     print(rectangle)
+    ellipse = figure.area_ellipse(width, height)
+    print(ellipse)
+    right_triangle = figure.area_right_triangle(width, height)
+    print(right_triangle)
 except ValueError:
     print("please input positive number for width and height")
